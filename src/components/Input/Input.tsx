78e702345f94d4d670842392e64425934e5cc8c2
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-09 12:58:57 +0300

    feat: add input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,16 @@
+import styles from './Input.module.css';
+import cn from 'classnames';
+import { InputProps } from './Input.props';
+
+const Input = ({ className, isValid = true, ...props }: InputProps) => {
+    return (
+        <input
+            className={cn(styles.input, className, {
+                [styles.invalid]: isValid,
+            })}
+            {...props}
+        />
+    );
+};
+
+export default Input;
