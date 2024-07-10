a14a11a1245b398e2a62bf06f6c8c62d13928a25
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-10 12:58:44 +0300

    feat: add routes for and provider for Tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AuthLayout from './layout/Auth/AuthLayout.tsx';
+import NotFound from './pages/NotFound/NotFound.tsx';
+import Layout from './layout/Menu/Layout.tsx';
+import RequireAuth from './helpers/RequireAuth.tsx';
+import ReactDOM from 'react-dom/client';
+import Register from './pages/Register/Register.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: (
+            <RequireAuth>
+                <Layout />
+            </RequireAuth>
+        ),
+        children: [
+            {
+                path: 'posts',
+                element: <></>,
+            },
+        ],
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <></>,
+            },
+            {
+                path: 'register',
+                element: <Register />,
+            },
+        ],
+    },
+    {
+        path: '/settings',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: 'personalData',
+                element: <></>,
+            },
+            {
+                path: 'password',
+                element: <></>,
+            },
+        ],
+    },
+    {
+        path: '*',
+        element: <NotFound />,
+    },
+]);
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
