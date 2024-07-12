2332bf6e074fb53dc2369103ee474dfe5a94f588
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-12 20:58:17 +0300

    feat: change usePublication from query to mutate function
    add category parameter

--- src/hooks/usePublications.ts
+++ src/hooks/usePublications.ts
@@ -1,17 +1,37 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import publicationService from '../api/services/publication.service';
+import { AxiosError } from 'axios';
 
 export const usePublications = () => {
-    return useQuery({
-        queryKey: ['publications'],
-        queryFn: async () => {
+    return useMutation({
+        mutationFn: async (category: string) => {
             const jwt = localStorage.getItem('jwt');
 
             if (!jwt) {
                 return;
             }
 
-            return await publicationService.getAllPublications(jwt);
+            const publications = await publicationService.getAllPublications(jwt);
+
+            if (category === '') {
+                return publications;
+            }
+
+            return publications.filter(
+                (publication) => publication.category.toLowerCase() === category.toLowerCase()
+            );
+        },
+        onSuccess: async (data, variables) => {
+            console.log('Successfully got publications by category');
+            console.log(data);
+            console.log(variables);
+        },
+        onError: async (error, variables) => {
+            console.log('Error while getting publications by category');
+            if (error instanceof AxiosError) {
+                console.log(error.response?.data.message);
+            }
+            console.log(variables);
         },
     });
 };
