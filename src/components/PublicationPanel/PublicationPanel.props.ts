d044b4b34e23542cb7894e09f20f6f389f8f74c5
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-12 21:00:58 +0300

    feat: change PublicationPanel component
    add function to change category of publications

--- src/components/PublicationPanel/PublicationPanel.props.ts
+++ src/components/PublicationPanel/PublicationPanel.props.ts
@@ -3,5 +3,6 @@ import { Publication } from '../../api/@types/api';
 export interface PublicationPanelProps extends React.ComponentPropsWithRef<'div'> {
     publications: Publication[];
     createPost: () => void;
+    setPostCategory: (category: string) => void;
     displayPost: (publication: Publication) => void;
 }
--- src/components/PublicationPanel/PublicationPanel.tsx
+++ src/components/PublicationPanel/PublicationPanel.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import CardButton from '../CardButton/CardButton';
 import Heading from '../Heading/Heading';
+import Input from '../Input/Input';
 import PublicationCartButton from '../PublicationCartButton/PublicationCartButton';
 import styles from './PublicationPanel.module.css';
 import { PublicationPanelProps } from './PublicationPanel.props';
 
-const PublicationPanel = ({ publications, createPost, displayPost }: PublicationPanelProps) => {
+const PublicationPanel = ({
+    publications,
+    createPost,
+    displayPost,
+    setPostCategory,
+}: PublicationPanelProps) => {
+    const [category, setCategory] = useState<string>('');
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCategory(e.target.value);
+    };
+
     return (
         <div className={styles.wrapper}>
             <Heading>Публикации</Heading>
+            <Input
+                className={styles.input}
+                value={category}
+                onChange={onChange}
+                placeholder='Поиск по категории'
+            />
+            <CardButton onClick={() => setPostCategory(category)}>Поиск по категории</CardButton>
             <CardButton onClick={createPost}>
                 <img src='./plus-icon.svg' alt='Создать новый пост' />
                 Создать новый пост
