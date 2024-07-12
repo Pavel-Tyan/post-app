8ef512a93c77ef4994fd5a7d70cffe7522ddc37a
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-12 08:53:18 +0300

    feat: add component for settings

---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Settings.module.css';
+import cn from 'classnames';
+
+const Settings = () => {
+    return (
+        <nav className={styles.navbar} aria-label='Навигация по настройкам пользователя'>
+            <ul className={styles.linkList}>
+                <li>
+                    <NavLink
+                        className={({ isActive }) =>
+                            cn(styles.link, {
+                                [styles.active]: isActive,
+                            })
+                        }
+                        to='/password'
+                    >
+                        Пароль
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        className={({ isActive }) =>
+                            cn(styles.link, {
+                                [styles.active]: isActive,
+                            })
+                        }
+                        to='/personalData'
+                    >
+                        Личные данные
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Settings;
